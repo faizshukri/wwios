d5cede506e585d72f2afc8e3b00f189da7591cc8
Faiz Shukri <faizshukri90@gmail.com>
2014-11-08 23:47:40 +0000

    Add settings page and change home button
    Signed-off-by: Faiz Shukri <faizshukri90@gmail.com>

---
+++ Wigly2/SettingsViewController.h
@@ -0,0 +1,20 @@
+//
+//  SettingsViewController.h
+//  Wigly2
+//
+//  Created by Faiz Shukri on 11/8/14.
+//  Copyright (c) 2014 Faiz Shukri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UITableViewController
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *speedControl;
+@property (weak, nonatomic) IBOutlet UISwitch *soundButton;
+@property (weak, nonatomic) IBOutlet UIStepper *wormholeStepper;
+@property (weak, nonatomic) IBOutlet UITextField *wormholeText;
+
+-(IBAction)saveDefault:(id)sender;
+-(IBAction)resetDefault:(id)sender;
+@end
---
+++ Wigly2/SettingsViewController.m
@@ -0,0 +1,40 @@
+//
+//  SettingsViewController.m
+//  Wigly2
+//
+//  Created by Faiz Shukri on 11/8/14.
+//  Copyright (c) 2014 Faiz Shukri. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction) resetDefault:(id)sender {
+    
+}
+
+-(IBAction) saveDefault:(id)sender {
+    
+}
+
+@end
